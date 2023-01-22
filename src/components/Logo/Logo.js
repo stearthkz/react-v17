95554e726c88079c7205c0c61a987f2d16e0a859
Sergey Tsepilov <stearthkz@mail.ru>
2023-01-22 12:15:23 +0600

    Logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import logo from "../../images/logo.svg";
+
+import "./Logo.css";
+
+function Logo() {
+  return (
+    <img src={logo} className="logo" alt="Логотип" />
+  );
+}
+
+export default Logo;
