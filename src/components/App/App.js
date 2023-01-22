d8694a298f6aa5ecf835f46137845718ea2ab546
Sergey Tsepilov <stearthkz@mail.ru>
2023-01-22 12:14:47 +0600

    App

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
